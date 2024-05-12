032d5f3db70e408a138637b0adce42d4c8aa80e0
Fardeen31 <ahmedfardeen96@gmail.com>
2024-05-12 14:57:15 -0400

    Added more changes

--- chat.c
+++ chat.c
@@ -7,6 +7,13 @@
 #include <openssl/evp.h>
 #include <openssl/hmac.h>
 #include <getopt.h>
+#include <string.h>
+#include <openssl/aes.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <arpa/inet.h> // For byte order conversions
+
 #include "dh.h"
 #include "keys.h"
 
@@ -14,18 +21,23 @@
 #define PATH_MAX 1024
 #endif
 
+mpz_t A_pk, A_sk, B_pk;
+
+// DHF
+char hmac_key[256+1];
+unsigned char aes_key[256];
+
+unsigned char iv_val[16+1];
+
 static GtkTextBuffer* tbuf; /* transcript buffer */
 static GtkTextBuffer* mbuf; /* message buffer */
 static GtkTextView*  tview; /* view for transcript */
 static GtkTextMark*   mark; /* used for scrolling to end of transcript, etc */
 
-static pthread_t trecv;     /* wait for incoming messagess and post to queue */
+static pthread_t trecv;     /* wait for incoming messages and post to queue */
 void* recvMsg(void*);       /* for trecv */
 
-#define max(a, b)         \
-	({ typeof(a) _a = a;    \
-	 typeof(b) _b = b;    \
-	 _a > _b ? _a : _b; })
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 /* network stuff... */
 
@@ -34,405 +46,124 @@ static int isclient = 1;
 
 static void error(const char *msg)
 {
-	perror(msg);
-	exit(EXIT_FAILURE);
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
 int initServerNet(int port)
 {
-	int reuse = 1;
-	struct sockaddr_in serv_addr;
-	listensock = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(listensock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-	/* NOTE: might not need the above if you make sure the client closes first */
-	if (listensock < 0)
-		error("ERROR opening socket");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(port);
-	if (bind(listensock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-		error("ERROR on binding");
-	fprintf(stderr, "listening on port %i...\n",port);
-	listen(listensock,1);
-	socklen_t clilen;
-	struct sockaddr_in  cli_addr;
-	sockfd = accept(listensock, (struct sockaddr *) &cli_addr, &clilen);
-	if (sockfd < 0)
-		error("error on accept");
-	close(listensock);
-	fprintf(stderr, "connection made, starting session...\n");
-	/* at this point, should be able to send/recv on sockfd */
-
-	/* ----------Handshake setup----------- */
-
-	//----- Sending SYN+1,ACK -----
-
-		/* Server sends SYN+1, ACK back to Client when SYN is recieved*/
-		char bufferSYN[11];
-		recv(sockfd, bufferSYN, 10, 0);
-		int bufferSYNp1 = atoi(bufferSYN) + 1;
-		string bufferSYNp1_str = std::to_string(bufferSYNp1) + "ACK";
-		const char *bufferSYNp1_char = bufferSYNp1_str.c_str();
-
-		/* Generate the same iv from ISN seed passed over*/
-		int bufferSYNiv = atoi(bufferSYN);
-		unsigned char* iv_temp = ivgen(bufferSYNiv);
-		memcpy(iv_val, iv_temp, 16);
-
-		// SYN recived should be within 32bit unsigned range
-		if (bufferSYNp1 >= 0 && bufferSYNp1 <= 4294967295)  {
-			// send(sockfd, bufferSYN, 11, 0);
-			send(sockfd, bufferSYNp1_char, bufferSYNp1_str.length(), 0);
-		} else {
-			error("Server failed to recieve SYN from client");
-		}
-
-		char buff[10];
-		recv(sockfd, buff, 10, 0);
-
-	/* ----------DiffieHellman Setup----------- */
-
-		//generate private & public keys
-		init("params");
-		NEWZ(a);
-		NEWZ(A);
-		dhGen(a, A);
-
-		//send public key
-		char S[1024];	
-		mpz_get_str(S, 16, A);
-		send(sockfd, S, 1024, 0);
-
-		mpz_set(A_pk, A);
-		mpz_set(A_sk, a);
-
-		//reieve incoming user's public key
-		char buf[1024];
-		recv(sockfd, buf, 1024, 0);
-		mpz_set_str(B_pk, buf, 16);
-			
-		//shared key is calcualted 
-		const size_t klen = 256;
-		unsigned char kA[klen];
-		dhFinal(A_sk, A_pk, B_pk, kA, klen);
-		char dhf[512+1];
-		for(size_t i=0; i < 256; i++) {
-			sprintf(&dhf[i*2], "%02x", kA[i]);
-		}
-
-		//split 512 DHF for two 256 keys (AES & HMAC)
-		memcpy(hmac_key, dhf, 256);
-		memcpy(aes_key, dhf + 256, 256);
-	return 0;
+    int reuse = 1;
+    struct sockaddr_in serv_addr;
+    listensock = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(listensock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    if (listensock < 0)
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+    if (bind(listensock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    fprintf(stderr, "listening on port %i...\n", port);
+    listen(listensock, 1);
+    socklen_t clilen;
+    struct sockaddr_in cli_addr;
+    sockfd = accept(listensock, (struct sockaddr *) &cli_addr, &clilen);
+    if (sockfd < 0)
+        error("error on accept");
+    close(listensock);
+    fprintf(stderr, "connection made, starting session...\n");
+    return 0;
 }
 
-static int initClientNet(char* hostname, int port)
+int initClientNet(char *hostname, int port)
 {
-	struct sockaddr_in serv_addr;
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	struct hostent *server;
-	if (sockfd < 0)
-		error("ERROR opening socket");
-	server = gethostbyname(hostname);
-	if (server == NULL) {
-		fprintf(stderr,"ERROR, no such host\n");
-		exit(0);
-	}
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
-	serv_addr.sin_port = htons(port);
-	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-		error("ERROR connecting");
-	/* at this point, should be able to send/recv on sockfd */
-	/* ----------Handshake setup----------- */
-
-	// ----- Sending SYN -----
-
-		/* ISN Generation - 32 bit unsigned max*/
-		srand(time(0));
-		unsigned long ISN = rand() % 4294967294 + 0;
-
-
-		/* ISN is used as seed to generate IV for AES*/
-		unsigned char* iv_temp = ivgen(ISN);
-		memcpy(iv_val, iv_temp, 16);
-
-		/* Foramted Initial Sequence Number for sending */
-		string ISN_str = std::to_string(ISN);
-		char const *ISN_char = ISN_str.c_str();
-
-		//memcpy(checking, ISN_char, ISN_str.length());
-
-		/* Client sends ISN as SYN request*/
-		send(sockfd, ISN_char, ISN_str.length(), 0);
-
-	// ----- Sending ACK -----
-
-		/* Client sends ACK when SYN+1, ACK is recieved back */
-		char bufferACK[14]; // size char to recieve the SYN+1, ACK from the server
-		recv(sockfd, bufferACK, 14, 0);
-		string bfA_str(bufferACK);
-
-		
-		size_t index = bfA_str.find("ACK");
-		while (index != std::string::npos) {
-			bfA_str.erase(index,3); // removes "ACK" from string
-			index = bfA_str.find("ACK", index);
-		}
-
-		unsigned long bufferACK_int = stoi(bfA_str) - 1; //convert str to int
-		
-		if(bufferACK_int == ISN) {
-			send(sockfd, "ACK", 3, 0);
-		} else {
-			error("Client failed to recieve SYN+1 ACK from server");
-		}
-
-	/* ----------DiffieHellman Setup----------- */
-
-		//generate private & public keys
-		init("params");
-		NEWZ(a);
-		NEWZ(A);
-		dhGen(a, A);
-
-		//recieve incoming user's public key
-		char buf[1024];
-		recv(sockfd, buf, 1024, 0);
-
-		mpz_set(A_pk, A);
-		mpz_set(A_sk, a);
-		mpz_set_str(B_pk, buf, 16);
-
-		//send public key
-		char S[1024];
-		mpz_get_str(S, 16, A);
-		send(sockfd, S, 1024, 0);
-
-		//shared key is calcualted
-		const size_t klen = 256;
-		unsigned char kA[klen];
-		dhFinal(A_sk, A_pk, B_pk, kA, klen);
-		char dhf[512+1];
-		for(size_t i=0; i < 256; i++) {
-			sprintf(&dhf[i*2], "%02x", kA[i]);
-		}
-
-		//split 512 DHF for two 256 key (AES & HMAC)
-		memcpy(hmac_key, dhf, 256);
-		memcpy(aes_key, dhf + 256, 256);
-	return 0;
+    struct sockaddr_in serv_addr;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct hostent *server = gethostbyname(hostname);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    serv_addr.sin_port = htons(port);
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+    return 0;
 }
 
 static int shutdownNetwork()
 {
-	shutdown(sockfd,2);
-	unsigned char dummy[64];
-	ssize_t r;
-	do {
-		r = recv(sockfd,dummy,64,0);
-	} while (r != 0 && r != -1);
-	close(sockfd);
-	return 0;
+    shutdown(sockfd, 2);
+    unsigned char dummy[64];
+    ssize_t r;
+    do {
+        r = recv(sockfd, dummy, 64, 0);
+    } while (r != 0 && r != -1);
+    close(sockfd);
+    return 0;
 }
 
-/* end network stuff. */
-
-
-static const char* usage =
-"Usage: %s [OPTIONS]...\n"
-"Secure chat (CCNY computer security project).\n\n"
-"   -c, --connect HOST  Attempt a connection to HOST.\n"
-"   -l, --listen        Listen for new connections.\n"
-"   -p, --port    PORT  Listen or connect on PORT (defaults to 1337).\n"
-"   -h, --help          show this message and exit.\n";
-
-/* Append message to transcript with optional styling.  NOTE: tagnames, if not
- * NULL, must have it's last pointer be NULL to denote its end.  We also require
- * that messsage is a NULL terminated string.  If ensurenewline is non-zero, then
- * a newline may be added at the end of the string (possibly overwriting the \0
- * char!) and the view will be scrolled to ensure the added line is visible.  */
 static void tsappend(char* message, char** tagnames, int ensurenewline)
 {
-	GtkTextIter t0;
-	gtk_text_buffer_get_end_iter(tbuf,&t0);
-	size_t len = g_utf8_strlen(message,-1);
-	if (ensurenewline && message[len-1] != '\n')
-		message[len++] = '\n';
-	gtk_text_buffer_insert(tbuf,&t0,message,len);
-	GtkTextIter t1;
-	gtk_text_buffer_get_end_iter(tbuf,&t1);
-	/* Insertion of text may have invalidated t0, so recompute: */
-	t0 = t1;
-	gtk_text_iter_backward_chars(&t0,len);
-	if (tagnames) {
-		char** tag = tagnames;
-		while (*tag) {
-			gtk_text_buffer_apply_tag_by_name(tbuf,*tag,&t0,&t1);
-			tag++;
-		}
-	}
-	if (!ensurenewline) return;
-	gtk_text_buffer_add_mark(tbuf,mark,&t1);
-	gtk_text_view_scroll_to_mark(tview,mark,0.0,0,0.0,0.0);
-	gtk_text_buffer_delete_mark(tbuf,mark);
+    GtkTextIter t0;
+    gtk_text_buffer_get_end_iter(tbuf, &t0);
+    size_t len = strlen(message);
+    if (ensurenewline && message[len - 1] != '\n')
+        message[len++] = '\n';
+    gtk_text_buffer_insert(tbuf, &t0, message, len);
+    GtkTextIter t1;
+    gtk_text_buffer_get_end_iter(tbuf, &t1);
+    t0 = t1;
+    gtk_text_iter_backward_chars(&t0, len);
+    if (tagnames) {
+        for (char** tag = tagnames; *tag != NULL; tag++) {
+            gtk_text_buffer_apply_tag_by_name(tbuf, *tag, &t0, &t1);
+        }
+    }
+    if (ensurenewline) {
+        gtk_text_buffer_add_mark(tbuf, mark, &t1);
+        gtk_text_view_scroll_to_mark(tview, mark, 0.0, FALSE, 0, 0);
+    }
 }
 
-static void sendMessage(GtkWidget* w /* <-- msg entry widget */, gpointer /* data */)
+static void sendMessage(GtkWidget* w, gpointer data)
 {
-	char* tags[2] = {"self",NULL};
-	tsappend("me: ",tags,0);
-	GtkTextIter mstart; /* start of message pointer */
-	GtkTextIter mend;   /* end of message pointer */
-	gtk_text_buffer_get_start_iter(mbuf,&mstart);
-	gtk_text_buffer_get_end_iter(mbuf,&mend);
-	char* message = gtk_text_buffer_get_text(mbuf,&mstart,&mend,1);
-	size_t len = g_utf8_strlen(message,-1);
-	/* XXX we should probably do the actual network stuff in a different
-	 * thread and have it call this once the message is actually sent. */
-	ssize_t nbytes;
-	if ((nbytes = send(sockfd,message,len,0)) == -1)
-		error("send failed");
-
-	tsappend(message,NULL,1);
-	free(message);
-	/* clear message text and reset focus */
-	gtk_text_buffer_delete(mbuf,&mstart,&mend);
-	gtk_widget_grab_focus(w);
-}
-
-static gboolean shownewmessage(gpointer msg)
-{
-	char* tags[2] = {"friend",NULL};
-	char* friendname = "mr. friend: ";
-	tsappend(friendname,tags,0);
-	char* message = (char*)msg;
-	tsappend(message,NULL,1);
-	free(message);
-	return 0;
+    char* tags[2] = {"self", NULL};
+    tsappend("me: ", tags, 0);
+    GtkTextIter mstart, mend;
+    gtk_text_buffer_get_start_iter(mbuf, &mstart);
+    gtk_text_buffer_get_end_iter(mbuf, &mend);
+    char* message = gtk_text_buffer_get_text(mbuf, &mstart, &mend, FALSE);
+    size_t len = strlen(message);
+    ssize_t nbytes = send(sockfd, message, len, 0);
+    if (nbytes == -1)
+        error("send failed");
+    tsappend(message, NULL, 1);
+    free(message);
+    gtk_text_buffer_set_text(mbuf, "", -1);
+    gtk_widget_grab_focus(w);
 }
 
 int main(int argc, char *argv[])
 {
-	if (init("params") != 0) {
-		fprintf(stderr, "could not read DH params from file 'params'\n");
-		return 1;
-	}
-	// define long options
-	static struct option long_opts[] = {
-		{"connect",  required_argument, 0, 'c'},
-		{"listen",   no_argument,       0, 'l'},
-		{"port",     required_argument, 0, 'p'},
-		{"help",     no_argument,       0, 'h'},
-		{0,0,0,0}
-	};
-	// process options:
-	char c;
-	int opt_index = 0;
-	int port = 1337;
-	char hostname[HOST_NAME_MAX+1] = "localhost";
-	hostname[HOST_NAME_MAX] = 0;
+    gtk_init(&argc, &argv);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *view = gtk_text_view_new();
+    GtkWidget *scroll = gtk_scrolled_window_new(NULL, NULL);
+    GtkWidget *entry = gtk_entry_new();
+    GtkWidget *send_button = gtk_button_new_with_label("Send");
 
-	while ((c = getopt_long(argc, argv, "c:lp:h", long_opts, &opt_index)) != -1) {
-		switch (c) {
-			case 'c':
-				if (strnlen(optarg,HOST_NAME_MAX))
-					strncpy(hostname,optarg,HOST_NAME_MAX);
-				break;
-			case 'l':
-				isclient = 0;
-				break;
-			case 'p':
-				port = atoi(optarg);
-				break;
-			case 'h':
-				printf(usage,argv[0]);
-				return 0;
-			case '?':
-				printf(usage,argv[0]);
-				return 1;
-		}
-	}
-	/* NOTE: might want to start this after gtk is initialized so you can
-	 * show the messages in the main window instead of stderr/stdout.  If
-	 * you decide to give that a try, this might be of use:
-	 * https://docs.gtk.org/gtk4/func.is_initialized.html */
-	if (isclient) {
-		initClientNet(hostname,port);
-	} else {
-		initServerNet(port);
-	}
+    gtk_container_add(GTK_CONTAINER(scroll), view);
+    gtk_container_add(GTK_CONTAINER(window), scroll);
+    gtk_container_add(GTK_CONTAINER(window), entry);
+    gtk_container_add(GTK_CONTAINER(window), send_button);
 
-	/* setup GTK... */
-	GtkBuilder* builder;
-	GObject* window;
-	GObject* button;
-	GObject* transcript;
-	GObject* message;
-	GError* error = NULL;
-	gtk_init(&argc, &argv);
-	builder = gtk_builder_new();
-	if (gtk_builder_add_from_file(builder,"layout.ui",&error) == 0) {
-		g_printerr("Error reading %s\n", error->message);
-		g_clear_error(&error);
-		return 1;
-	}
-	mark  = gtk_text_mark_new(NULL,TRUE);
-	window = gtk_builder_get_object(builder,"window");
-	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	transcript = gtk_builder_get_object(builder, "transcript");
-	tview = GTK_TEXT_VIEW(transcript);
-	message = gtk_builder_get_object(builder, "message");
-	tbuf = gtk_text_view_get_buffer(tview);
-	mbuf = gtk_text_view_get_buffer(GTK_TEXT_VIEW(message));
-	button = gtk_builder_get_object(builder, "send");
-	g_signal_connect_swapped(button, "clicked", G_CALLBACK(sendMessage), GTK_WIDGET(message));
-	gtk_widget_grab_focus(GTK_WIDGET(message));
-	GtkCssProvider* css = gtk_css_provider_new();
-	gtk_css_provider_load_from_path(css,"colors.css",NULL);
-	gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
-			GTK_STYLE_PROVIDER(css),
-			GTK_STYLE_PROVIDER_PRIORITY_USER);
-
-	/* setup styling tags for transcript text buffer */
-	gtk_text_buffer_create_tag(tbuf,"status","foreground","#657b83","font","italic",NULL);
-	gtk_text_buffer_create_tag(tbuf,"friend","foreground","#6c71c4","font","bold",NULL);
-	gtk_text_buffer_create_tag(tbuf,"self","foreground","#268bd2","font","bold",NULL);
-
-	/* start receiver thread: */
-	if (pthread_create(&trecv,0,recvMsg,0)) {
-		fprintf(stderr, "Failed to create update thread.\n");
-	}
-
-	gtk_main();
-
-	shutdownNetwork();
-	return 0;
-}
-
-/* thread function to listen for new messages and post them to the gtk
- * main loop for processing: */
-void* recvMsg(void*)
-{
-	size_t maxlen = 512;
-	char msg[maxlen+2]; /* might add \n and \0 */
-	ssize_t nbytes;
-	while (1) {
-		if ((nbytes = recv(sockfd,msg,maxlen,0)) == -1)
-			error("recv failed");
-		if (nbytes == 0) {
-			/* XXX maybe show in a status message that the other
-			 * side has disconnected. */
-			return 0;
-		}
-		char* m = malloc(maxlen+2);
-		memcpy(m,msg,nbytes);
-		if (m[nbytes-1] != '\n')
-			m[nbytes++] = '\n';
-		m[nbytes] = 0;
-		g_main_context_invoke(NULL,shownewmessage,(gpointer)m);
-	}
-	return 0;
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
 }
